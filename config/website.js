0c74cbbc6b82537953a95a31457d84ea6e3ec1ae
Abel Quintero <abel.j.quintero@gmail.com>
2019-04-16 19:14:32 -0700

    updating project images

---
+++ config/website.js
@@ -0,0 +1,22 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+  siteTitle: 'Abel Quintero', // Navigation and Site Title
+  siteTitleAlt: 'Abel Quintero - Portfolio', // Alternative Site title for SEO
+  siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
+  siteTitleShort: 'Abel', // short_name for manifest
+  siteUrl: 'https://ajquintero.github.io/abel-quintero.github.io/', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logo.png', // Used for SEO and manifest
+  siteDescription: 'Minimalistic bright portfolio with full-width grid and large images',
+  author: 'LekoArts', // Author for schema.org JSONLD
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@emma', // Twitter Username
+  ogSiteName: 'emma', // Facebook Site Name
+  ogLanguage: 'en_US',
+  googleAnalyticsID: 'UA-47519312-3',
+
+  // Manifest and Progress color
+  themeColor: '#3498DB',
+  backgroundColor: '#2b2e3c',
+}
